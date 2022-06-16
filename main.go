242ce68204f7f707ed3ee8e6f1cfd15e2c535e77
r0nk <r0nk@simplecpu.com>
2022-06-15 20:08:34 -0500

    somewhat usable version

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
@@ -17,8 +19,11 @@ type edge struct {
 }
 
 func run_edge_script(e edge, cause string) {
-	//set environmental variable to changing file
-	fmt.Printf("running edge script" + e.output)
+
+	//TODO pipe the cause file into standard input for the program
+
+	fmt.Printf("%s %s %s (%s)\n", e.listen_regex, e.script, e.output, cause)
+	// set environmental variable to changing file
 	out, err := exec.Command(e.script, cause).Output()
 	if err != nil {
 		log.Fatal(err)
@@ -40,14 +45,17 @@ func handle_edge(e edge) {
 
 	//TODO do this for every file that the regex matches
 	// it currently only does direct matches
+	//TODO handle multiple connection cases
 	watcher.Add(e.listen_regex)
+	fmt.Printf("Listening to %s\n", e.listen_regex)
+
 	for {
 		select {
 		case event, ok := <-watcher.Events:
 			if !ok {
 				return
 			}
-			log.Println("event:", event)
+			//log.Println("event:", event)
 			if event.Op&fsnotify.Write == fsnotify.Write {
 				go run_edge_script(e, event.Name)
 			}
@@ -58,30 +66,38 @@ func handle_edge(e edge) {
 			log.Println("error:", err)
 		}
 	}
-	//TODO handle multiple connection cases
-	fmt.Printf("asdf", e)
 }
 
 func read_edges() []edge {
+	crystalfile, err := os.Open("crystalfile")
+	if err != nil {
+		log.Fatal(err)
+	}
 	var ret []edge
-	//for each entry in crystalfile
-	//read entry
-	//add to ret
-	//return ret
+
+	fscan := bufio.NewScanner(crystalfile)
+	fscan.Split(bufio.ScanLines)
+
 	var e edge
-	e.listen_regex = "nodes/from.txt"
-	e.output = "nodes/to.txt"
-	e.script = "edges/count"
-	ret = append(ret, e)
+	for fscan.Scan() {
+		fields := strings.Fields(fscan.Text())
+		e.listen_regex = fields[0]
+		e.script = fields[1]
+		e.output = fields[2]
+		ret = append(ret, e)
+	}
+
+	//TODO do some basic sanity checks on the graph
+	// (infinite loops and the like)
+
 	return ret
 }
 
 func main() {
-	fmt.Printf("starting up...")
 	for _, edge := range read_edges() {
 		go handle_edge(edge)
 	}
-	for { //watch edges
+	for { //TODO watch crystal file for changes
 		time.Sleep(1 * time.Second)
 	}
 }
